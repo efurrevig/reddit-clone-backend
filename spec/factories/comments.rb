c3bae26283cb49cc73df11b2e75648ab92f2c49b
Eric Furrevig <efurrevig@gmail.com>
2023-07-14 22:23:01 -0400

    Fixed comment tests

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -7,6 +7,7 @@ FactoryBot.define do
 
     factory :comment_of_post do
       association :commentable, factory: :post
+      root_id { commentable.id }
     end
 
     factory :comment_of_comment do
