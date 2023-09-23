25da398fb3c84ff6df50f4c55bc8708bd59cec51
Eric Furrevig <efurrevig@gmail.com>
2023-09-23 16:24:02 -0400

    Test

--- db/migrate/20230619225911_create_comments.rb
+++ db/migrate/20230619225911_create_comments.rb
@@ -2,7 +2,6 @@ class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
       t.references :users, null: false, foreign_key: true
-      t.references :posts, null: false, foreign_key: true
       t.integer :parent_comment_id, foreign_key: { to_table: :comments }
       t.text :body, null: false
       t.boolean :is_deleted, null: false, default: false
